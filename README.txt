d291f1fec6631f734f659247407c971cc045e965
dibu1994 <ce1130340@civil.iitd.ac.in>
2017-06-28 15:17:06 +0530

    c

--- README.txt
+++ README.txt
@@ -2,9 +2,47 @@ To run application open index.html in your browser
 
 Optimzations required
 
- index.html
+ FIle : index.html
  - add async attribute to analytics and perfmatters script
  - replace images with compressed images. COmpressed version of images is located at compressed/images
  - optimize css delievery for screen , print and fonts styles
- - add async tags in images 
+ - add async tags in images for asynchronous loading of images
 
+
+ File:  views/js/main.js
+
+-Changed changedPizzaSizes function :- Pipeline is not broken now and Expensive determinDx method not called now
+
+function changePizzaSizes(size) {
+
+var newWidth=0;
+    switch(size)
+    {
+      case "1":
+      newWidth=25;
+      break;
+      case "2":
+      newWidth=33.33;
+      break;
+      case "3":
+      newWidth=50;
+      break;
+      default:
+      console.log("Bug");
+    }
+    var randomPizzas=document.querySelectorAll(".randomPizzaContainer");
+
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      randomPizzas[i].style.width = newWidth+"%";
+    }
+  }
+
+  -in updatePositions functions take out  line :var top=document.body.scrollTop/1250 ouside for loop so that pipeline is not broken
+  - query Selector replaced by getElementById in function changeSliderLabel
+  - At Line 550 number of pizzas reduced to 24 insted of 200
+  - This declaration is made outside for loop :  var movingPizzas = document.getElementById('movingPizzas1');
+
+
+  File : views/css/style.css
+
+  Added   transform: translateZ(0); to mover class
--- views/js/main.js
+++ views/js/main.js
@@ -403,16 +403,17 @@ var resizePizzas = function(size) {
   window.performance.mark("mark_start_resize");   // User Timing API function
 
   // Changes the value for the size of the pizza above the slider
+  //query Selector replaced by getElementById
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        document.getElementById("pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        document.getElementById("pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -467,6 +468,7 @@ var newWidth=0;
     }
     var randomPizzas=document.querySelectorAll(".randomPizzaContainer");
 
+    // Pipeline not broken .. not calling determinDx function now which was breaking pipeline earlier
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       randomPizzas[i].style.width = newWidth+"%";
     }
@@ -518,9 +520,11 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var top=document.body.scrollTop;
+
+  // Taking out document.body.scrollTop outside for loop doesnot break pipeline now
+  var top=document.body.scrollTop/1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(( top/ 1250) + (i % 5));
+    var phase = Math.sin(top + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -541,7 +545,12 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+
+// This declaration is made outside for loop
+var movingPizzas = document.getElementById('movingPizzas1');
+
+  //number of pizzas reduced to 24 
+  for (var i = 0; i < 24; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -549,7 +558,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
